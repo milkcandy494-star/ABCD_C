84066809d127bea9f759cb7c105b44830866f503
milkcandy494-star <milkcandy494@gmail.com>
2025-09-30 10:11:28 +0900

    Add files via upload

--- 2025.09.30/matplotlib-2.py
+++ 2025.09.30/matplotlib-2.py
@@ -10,7 +10,7 @@ print(x)
 plt.title("Bar Graph", fontsize = 20, fontweight='bold', loc = 'center', color = 'blue')
 
 colors = ['red' if val > 7 else 'blue' for val in y]
-plt.bar(x, y, width=0.4, color="blue")
+plt.bar(x, y, width=0.4, color = colors)
 plt.xlabel('Sequence', fontsize = 12, fontweight='bold')
 plt.ylabel('Time(secs)', fontsize = 12, fontweight='bold')
 
